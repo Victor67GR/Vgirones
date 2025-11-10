e9711bb932cbafaf6f9d87169075c29b34e8d894
Victor67GR <vgirones@iesjulioantonio.cat>
2025-11-10 09:57:12 +0100

    Delete RA2 5 directory

--- RA2 5/RA2_2.5_4.2_VictorGirones.py
+++
@@ -1,9 +0,0 @@
-# Nom i cognoms: Victor Girones De Asix
-# Data: 06/11/2025
-# Descripció:
-# Escriu un programa que simuli llençar un dau (1-6) usant el mòdul random i prova de fer una funció que llenci el dau n vegades i mostri la mitjana.
-
-import random
-
-n = 10
-print("Mitjana:", sum(random.randint(1, 6) for _ in range(n)) / n)
